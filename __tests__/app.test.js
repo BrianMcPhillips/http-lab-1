b0c85a1742ca21e5b10d2bf1554b153e1e8cec42
Brian McPhillips <B.McPhillips89@gmail.com>
2020-09-29 01:44:25 -0700

    echo and red routes tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,4 +8,19 @@ describe('createResponse', () => {
     expect(response.text).toEqual('hi');
   });
 
+  it('returns plain text with post to echo', async() => {
+    const response = await request(app)
+      .post('/echo')
+      .send('This is the echo');
+    
+   
+    expect(response.text).toEqual('This is the echo');
+  });
+
+  it('returns red h1', async() => {
+    const response = await request(app)
+      .get('/red');
+    expect(response.text).toEqual('<h1>red</h1>');
+  });
+
 });
